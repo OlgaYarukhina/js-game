109e456e92fa82e6b00ec45912737e5554118980
Olha <olga.yarukhina@gmail.com>
2023-04-02 23:06:51 +0300

    game fild

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const crazyMan = document.getElementById("crazyMan");
+let dirMoveBoy;
+
+document.addEventListener("keydown", moveMan);
+
+function moveMan(event){
+     if(event.keyCode == 37) dirMoveBoy = "left";
+     else if (event.keyCode == 39) dirMoveBoy = "right";
+}
+
+
 // var i=0;
 
 // function dayLightChange(){
