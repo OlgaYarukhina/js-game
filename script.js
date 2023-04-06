a0ab81fe4b7a9a6a0601ed290e846e9eac791df8
oyarukhi <olga.yarukhina@gmail.com>
2023-04-06 17:32:55 +0300

    clouds move

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ let gameOver = false;
 
 let arr = [];
 let cloud = [][arr];
-cloudLight = [[10, 10], [11, 10], [9, 11], [10, 11], [11, 11], [12, 11]];
-cloudDark = [[10, 20], [11, 20], [9, 21], [10, 21], [11, 21], [12, 21]];
+let cloudNew = [[2, 1], [3, 1], [1, 2], [2, 2], [3, 2], [4, 2]]
+let cloudLight = [[10, 10], [11, 10], [9, 11], [10, 11], [11, 11], [12, 11]];
+let cloudDark = [[10, 20], [11, 20], [9, 21], [10, 21], [11, 21], [12, 21]];
 
-let crazyBoy = [27, 55];
+let clouds = [[[7, 1], [8, 1], [6, 2], [7, 2], [8, 2], [9, 2]], [[12, 1], [13, 1], [11, 2], [12, 2], [13, 2], [14, 2]]]
+let cloudsNumber = 10;
+
+let crazyBoy = [29, 55];
 let bulletNumber = 10;
 let bulletStart = crazyBoy.slice();
 let bullets = [bulletStart];
@@ -18,9 +22,8 @@ let bulletMove = [0, 0];
 const draw = () => {
     if (gameOver) return handleGameOver();
 
-    let htmlMarkup = `<div class ="crazyBoy" style="grid-area: ${crazyBoy[1]} / ${crazyBoy[0]}"></div>`;
+    let htmlMarkup = `<div class ="platform" >STOP!</div>`;
 
-    htmlMarkup += `<div class ="img" style="grid-area: ${crazyBoy[1]} / ${crazyBoy[0]}"></div>`;
 
     if (bullets.length > 1) {
         for (let i = 1; i < bullets.length; i++) {
@@ -31,14 +34,19 @@ const draw = () => {
                 bullets.length--;
                 delete bullets[i];
                 break
-               }
+            }
         }
     }
 
 
 
-   htmlMarkup += `<div class ="crazyBoy" style="grid-area: ${crazyBoy[1]} / ${crazyBoy[0]}"></div>`;
+    htmlMarkup += `<div class ="crazyBoy" style="grid-area: ${crazyBoy[1]} / ${crazyBoy[0]}"></div>`;
 
+    for (let i = 0; i < clouds.length; i++) {
+        for (let j = 0; j < clouds[i].length; j++) {
+            htmlMarkup += `<div class ="cloudLight" style="grid-area: ${clouds[i][j][1]} / ${clouds[i][j][0]}"></div>`;
+        }
+    }
 
     for (let i = 0; i < cloudLight.length; i++) {
         htmlMarkup += `<div class ="cloudLight" style="grid-area: ${cloudLight[i][1]} / ${cloudLight[i][0]}"></div>`;
@@ -66,14 +74,41 @@ const moveCloud = () => {
         }
     }
 
-    if (cloudLight[0][1] > 55 && cloudLight[0][0] === 20) { //if cloud too low it start rain
+    if (cloudLight[0][1] > 15 && cloudLight[0][0] === 20) { //if cloud too low it start rain
         gameOver = true;
     }
+}
 
+
+const moveClouds = () => {
+    for (let i = 0; i < clouds.length; i++) {
+        for (let j = 0; j < clouds[i].length; j++) {
+            clouds[i][j][0] += 1
+
+            if (clouds[i][j][0] > 55) {
+                for (let k = 0; k < clouds[i].length; k++) {
+                    clouds[i][k][1] += 4
+                    clouds[i][k][0] -= 50
+                }
+            }
+        }
+    }
+}
+
+const createNewCloud = () => {
+    if (cloudsNumber > 0) {
+    clouds.push(cloudNew);
+    cloudNew = [[2, 1], [3, 1], [1, 2], [2, 2], [3, 2], [4, 2]];
+    cloudsNumber--;
+    }
 }
 
 
 setInterval(moveCloud, 100);
+setInterval(moveClouds, 100);
+setInterval(createNewCloud, 500);
+
+
 
 const moveCrazyBoy = (e) => {
     if (e.key === "ArrowLeft" && crazyBoy[0] > 5) {
@@ -86,7 +121,6 @@ const moveCrazyBoy = (e) => {
 }
 
 const shoot = (e) => {
-   
     if (e.key === 'ArrowUp' && bulletNumber > 0) {
         bulletMove[1] = -1;
         bulletNumber--;
@@ -136,3 +170,6 @@ const handleGameOver = () => {
 
 // setInterval(dayLightChange, 2000);
 //<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" defer></script>
+
+  //    htmlMarkup += `<div class ="cloud" style="grid-area: ${20} / ${20}; grid-column-start: span 4; 
+    //    grid-row-start: span 2;"></div>`;
