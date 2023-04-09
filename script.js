274f9e9843ea0324fdc05d7e45d8bf25e1306b79
Olha <olga.yarukhina@gmail.com>
2023-04-09 17:27:11 +0300

    counters

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const playBoard = document.querySelector(".play-board");
 let setIntervalId;
 let gameOver = false;
+let timer = 60;
+let minutes, seconds;
+minutes = Math.floor(timer / 60);
+seconds = timer % 60;
 
 let arr = [];
 let cloud = [][arr];
@@ -8,10 +12,10 @@ let cloudNew = [[2, 1], [3, 1], [1, 2], [2, 2], [3, 2], [4, 2]]
 let cloudLight = [[10, 10], [11, 10], [9, 11], [10, 11], [11, 11], [12, 11]];
 
 let clouds = [[2, 1], [3, 1], [1, 2], [2, 2], [3, 2], [4, 2]]
-let cloudsNumber = 10;
+let cloudsNumber = 50;
 
 let crazyBoy = [29, 55];
-let bulletNumber = 10;
+let bulletNumber = 50;
 let bulletStart = crazyBoy.slice();
 let bullets = [bulletStart];
 let bulletMove = [0, 0];
@@ -20,34 +24,34 @@ let storm;
 
 
 const draw = () => {
-    if (gameOver) return handleGameOver();
+    if (gameOver || timer === -1) return handleGameOver();
 
-    let htmlMarkup = `<div class ="platform" >STOP!</div>`;
+    let htmlMarkup = `<div class ="platform">STOP!</div>`;
 
     htmlMarkup += `<div class ="crazyBoy" style="grid-area: ${crazyBoy[1]} / ${crazyBoy[0]}"></div>`;
 
     for (let i = 0; i < clouds.length; i++) {
-        if (clouds[i] !== undefined){
-        for (let j = 0; j < clouds[i].length; j++) {
-            if (clouds[i][j][1] < 10) { 
-                htmlMarkup += `<div class ="cloudLight" style="grid-area: ${clouds[i][j][1]} / ${clouds[i][j][0]}"></div>`;
-            } else if ((clouds[i][j][1] < 15)) {
-                htmlMarkup += `<div class ="cloudGrey" style="grid-area: ${clouds[i][j][1]} / ${clouds[i][j][0]}"></div>`;
-            } else {
-                if (i === storm) {
-                    htmlMarkup += `<div class ="storm" style="grid-area: ${clouds[i][j][1]} / ${clouds[i][j][0]}"></div>`;
+        if (clouds[i] !== undefined) {
+            for (let j = 0; j < clouds[i].length; j++) {
+                if (clouds[i][j][1] < 10) {
+                    htmlMarkup += `<div class ="cloudLight" style="grid-area: ${clouds[i][j][1]} / ${clouds[i][j][0]}"></div>`;
+                } else if ((clouds[i][j][1] < 15)) {
+                    htmlMarkup += `<div class ="cloudGrey" style="grid-area: ${clouds[i][j][1]} / ${clouds[i][j][0]}"></div>`;
                 } else {
-                    htmlMarkup += `<div class ="cloudDark" style="grid-area: ${clouds[i][j][1]} / ${clouds[i][j][0]}"></div>`;
+                    if (i === storm) {
+                        htmlMarkup += `<div class ="storm" style="grid-area: ${clouds[i][j][1]} / ${clouds[i][j][0]}"></div>`;
+                    } else {
+                        htmlMarkup += `<div class ="cloudDark" style="grid-area: ${clouds[i][j][1]} / ${clouds[i][j][0]}"></div>`;
+                    }
                 }
             }
         }
     }
-    }
 
-    for (let i = 0; i < cloudLight.length; i++) {
-        htmlMarkup += `<div class ="cloudLight" style="grid-area: ${cloudLight[i][1]} / ${cloudLight[i][0]}"></div>`;
-    }
-  
+    // for (let i = 0; i < cloudLight.length; i++) {
+    //     htmlMarkup += `<div class ="cloudLight" style="grid-area: ${cloudLight[i][1]} / ${cloudLight[i][0]}"></div>`;
+    // }
+
 
     if (bullets.length > 1) {
         for (let k = 1; k < bullets.length; k++) {
@@ -60,23 +64,32 @@ const draw = () => {
             }
 
             for (let i = 0; i < clouds.length; i++) {
-                if (clouds[i] !== undefined){
-                for (let j = 0; j < clouds[i].length; j++) {
-                    
-                    if (bullets[k][1] === clouds[i][j][1] && bullets[k][0] === clouds[i][j][0]) {
-                        for (let k = 0; k < clouds[i].length; k++) {
-                            htmlMarkup += `<div class ="cloudPurpul" style="grid-area: ${clouds[i][k][1]} / ${clouds[i][k][0]}"></div>`;
+                if (clouds[i] !== undefined) {
+                    for (let j = 0; j < clouds[i].length; j++) {
+
+                        if (bullets[k][1] === clouds[i][j][1] && bullets[k][0] === clouds[i][j][0]) {
+                            for (let k = 0; k < clouds[i].length; k++) {
+                                htmlMarkup += `<div class ="cloudPurpul" style="grid-area: ${clouds[i][k][1]} / ${clouds[i][k][0]}"></div>`;
+                                bullets.slice(k, 1)
+                            }
+                            //clouds.slice(i, 1)
+                            // clouds.length--;
+                            delete clouds[i]
+                            break;
                         }
-                        //clouds.slice(i, 1)
-                        // clouds.length--;
-                        delete clouds[i]
-                        break;
+
+                        if (bullets[k][1] === clouds[i][j][1] && bullets[k][0] === clouds[i][j][0] && i === storm) {
+                            return handleGameOver();
+                        }
+
                     }
                 }
             }
         }
-        }
     }
+
+    htmlMarkup += `<div class ="timer">${minutes}:${seconds}</div>`;
+    htmlMarkup += `<div class ="countBullets">Bullets: ${bulletNumber}</div>`;
 
     playBoard.innerHTML = htmlMarkup;
 }
@@ -85,38 +98,25 @@ const draw = () => {
 draw();
 
 
-const moveCloud = () => {
-    for (let i = 0; i < cloudLight.length; i++) {
-        cloudLight[i][0] += 1
-    }
-
-    if (cloudLight[0][0] > 55) {
-        for (let i = 0; i < cloudLight.length; i++) {
-            cloudLight[i][1] += 4
-            cloudLight[i][0] -= 50
-        }
-    }
-
-    if (cloudLight[0][1] > 40 && cloudLight[0][0] === 20) { //if cloud too low it start rain
-        gameOver = true;
-    }
-}
 
 
 const moveClouds = () => {
     for (let i = 0; i < clouds.length; i++) {
-        if (clouds[i] !== undefined){
-        for (let j = 0; j < clouds[i].length; j++) {
-            clouds[i][j][0] += 1
-            if (clouds[i][j][0] > 55) {
-                for (let k = 0; k < clouds[i].length; k++) {
-                    clouds[i][k][1] += 4
-                    clouds[i][k][0] -= 50
+        if (clouds[i] !== undefined) {
+            for (let j = 0; j < clouds[i].length; j++) {
+                clouds[i][j][0] += 1
+                if (clouds[i][j][0] > 55) {
+                    for (let k = 0; k < clouds[i].length; k++) {
+                        clouds[i][k][1] += 4
+                        clouds[i][k][0] -= 50
+                    }
                 }
             }
         }
     }
-}
+    if (clouds[0][0][0] === 20 && clouds[0][0][1] === 20) { //if cloud too low it start rain
+        gameOver = true;
+      }
 }
 
 const createNewCloud = () => {
@@ -128,14 +128,20 @@ const createNewCloud = () => {
 }
 
 const thunderstormCloud = () => {
-storm = Math.floor(Math.random()*10)+1;
+    storm = Math.floor(Math.random() * 10) + 1;
 }
 
+const timerCountDown = () => {
+    timer--;
+    minutes = Math.floor(timer / 60);
+    seconds = timer % 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+}
 
-setInterval(moveCloud, 80);
+setInterval(timerCountDown, 1000);
 setInterval(moveClouds, 80);
-setInterval(thunderstormCloud, 400);
-setInterval(createNewCloud, 400);
+setInterval(thunderstormCloud, 600);
+setInterval(createNewCloud, 500);
 setIntervalId = setInterval(draw, 10);
 
 
@@ -201,3 +207,23 @@ const handleGameOver = () => {
 
   //    htmlMarkup += `<div class ="cloud" style="grid-area: ${20} / ${20}; grid-column-start: span 4;
     //    grid-row-start: span 2;"></div>`;
+
+
+
+
+// const moveCloud = () => {
+//     for (let i = 0; i < cloudLight.length; i++) {
+//         cloudLight[i][0] += 1
+//     }
+
+//     if (cloudLight[0][0] > 55) {
+//         for (let i = 0; i < cloudLight.length; i++) {
+//             cloudLight[i][1] += 4
+//             cloudLight[i][0] -= 50
+//         }
+//     }
+
+//     if (cloudLight[0][1] > 40 && cloudLight[0][0] === 20) { //if cloud too low it start rain
+//         gameOver = true;
+//     }
+// }
